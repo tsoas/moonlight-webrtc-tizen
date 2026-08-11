d96fb859e195b069a726cedcae0a213da391836c
tsoas <40236452+tsoas@users.noreply.github.com>
2026-08-11 14:38:50 +0200

    Add libdatachannel PeerConnection

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+#include <rtc/rtc.hpp>
+
 int main()
 {
-    std::cout << "Moonlight WebRTC toolchain OK\n";
+    rtc::Configuration configuration;
+    rtc::PeerConnection peerConnection(configuration);
+
+    peerConnection.onStateChange([](rtc::PeerConnection::State state) {
+        std::cout << "PeerConnection state: " << state << '\n';
+    });
+
+    std::cout << "libdatachannel PeerConnection created OK\n";
     return 0;
 }
